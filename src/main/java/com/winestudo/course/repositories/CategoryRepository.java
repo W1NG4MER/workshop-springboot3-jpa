e29629f9aeb235096a0627d14f9a42b32a6a599b
William <wingamer.oliveiradossantos26@gmail.com>
2023-08-28 10:05:51 -0300

    Product

--- src/main/java/com/winestudo/course/repositories/CategoryRepository.java
+++ src/main/java/com/winestudo/course/repositories/CategoryRepository.java
@@ -3,7 +3,6 @@ package com.winestudo.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.winestudo.course.entities.Category;
-import com.winestudo.course.entities.User;
 
 public interface CategoryRepository extends JpaRepository<Category, Long>{
     
